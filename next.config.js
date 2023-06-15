5314a184da9377f03a63c2328934a8aa78d578b6
hasanma1ik <hasankhanmalik@gmail.com>
2023-06-16 01:44:14 +0500

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/NextJS-portfolio2' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/NextJS-portfolio2' : '',
 }
 
 module.exports = nextConfig
